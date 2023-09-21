8971deff23eeb768cb939c49760afa6f7c4cf379
PrateekJanagouda <126542602+PrateekJanagouda@users.noreply.github.com>
2023-09-21 12:05:53 +0530

    Add files via upload

---
+++ Smart_farm/components/data.js
@@ -0,0 +1,76 @@
+let products = [
+    { 
+        name:"Epsom salt", 
+        description:"Divine Tree Epsom Salt for Plants | Magnesium Sulfate Organic Fertilizer for Speed Up Plant Growth - 900 Gm",
+        id:"Epsom salt",
+        image:"https://m.media-amazon.com/images/I/61zjUYjw51L._SL1100_.jpg",
+
+
+        name:"Vermi compost", description:"TrustBasket Organic Vermicompost Fertilizer Manure For Plants 5 Kg Green",
+        id:"Vermi compost",image : "https://m.media-amazon.com/images/I/71zxEAIaI7L._AC_UL600_FMwebp_QL65_.jpg",
+
+        name:"ugaoo ",description : "Ugaoo Organic Vermicompost Fertilizer for Plants - 5 Kg",
+        id : "ugaoo ",image : "https://m.media-amazon.com/images/I/615ogT1U-KL._SX342_.jpg",
+
+
+        name:"NPK" ,description : "Go Garden NPK 19 19 19 Fertilizer Plant Food for Plants and Gardening Purpose - 450 Grams",
+        id : "NPK",image : "https://m.media-amazon.com/images/I/61d02TxNGPL._SX342_.jpg",
+
+            name: "Epsom salt",
+              description: "Divine Tree Epsom Salt for Plants | Magnesium Sulfate Organic Fertilizer for Speed Up Plant Growth - 900 Gm",
+              id: "Epsom salt",
+              image: "https://m.media-amazon.com/images/I/61zjUYjw51L._SL1100_.jpg",
+        
+              name: "Miracle-Gro Performance Organics All Purpose Plant Food",
+              description: "Water-soluble fertilizer that is easy to use and provides a balanced formula of nitrogen, phosphorus, and potassium for all types of plants. OMRI-listed for organic gardening.",
+              id: "MG00118842",
+              image: "https://m.media-amazon.com/images/I/413u84l7YUL._SL1500_.jpg",
+              name: "Espoma Garden-tone",
+              description: "Slow-release fertilizer that is perfect for vegetable gardens, flowers, and trees. Made with natural ingredients and will not burn your plants.",
+              id: "002091000003",
+              image: "https://m.media-amazon.com/images/I/616i86q478L._SL1500_.jpg"
+            ,
+            
+              name: "Jobe's Organics All Purpose Fertilizer",
+              description: "Granular fertilizer that is easy to apply and will feed your plants for up to 3 months. OMRI-listed for organic gardening.",
+              id: "071755000004",
+              image: "https://m.media-amazon.com/images/I/61-68m83gTL._SL1500_.jpg"
+            ,
+            
+              "name": "Schultz All Purpose Fertilizer",
+              "description": "Water-soluble fertilizer that is perfect for houseplants and container gardens. Also available in a slow-release formula.",
+              "id": "014851100178",
+              image: "https://m.media-amazon.com/images/I/4171iX8sF2L._SL1500_.jpg"
+            ,
+            
+              name: "General Hydroponics Flora Series",
+              description: "Balanced fertilizer that is specifically designed for hydroponics systems. Available in a variety of formulas to meet the needs of different plants.",
+              id: "GH10000052",
+              image: "https://m.media-amazon.com/images/I/71u52y4f69L._SL1500_.jpg"
+            ,
+            
+              name: "FoxFarms Big Bloom",
+              description: "Fertilizer that is specifically designed for flowering plants. It helps to promote bud growth and flowering.",
+              id: "FF0004",
+              image: "https://m.media-amazon.com/images/I/417k2789YSL._SL1500_.jpg"
+            ,
+
+            
+              name: "Greensand",
+              description: "Fertilizer that is rich in minerals and trace elements. It helps to improve soil health and plant growth.",
+              id: "GRNS001",
+              image: "https://m.media-amazon.com/images/I/41Yx9u7o4dL._SL1500_.jpg"
+            ,
+            
+              name: "Kelp Meal",
+              description: "Fertilizer that is rich in potassium and other nutrients. It helps to promote plant growth and flowering.",
+              id: "KELP001",
+              image: "https://m.media-amazon.com/images/I/410X59o32FL._SL1500_.jpg"
+            ,
+          
+``
+
+
+    }
+        
+]
